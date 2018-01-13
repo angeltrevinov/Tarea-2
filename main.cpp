0af91e7246bdf7a1d8a539fc4d17ba614da9191d
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-01-13 17:48:17 -0600

    there are still some erros, gonna check it

--- main.cpp
+++ main.cpp
@@ -8,9 +8,34 @@
 
 using namespace std;
 
+int BinarySearchTree(string &digitos, int min, int max){
+  int med = (max - min) / 2;
+  if(med == max || med == min)
+    return digitos.length();
+  else if(digitos[med] != digitos[med+1] && med+1 <= digitos.length()-1 ){//si se encuentra una pareja a la derecha
+    digitos.erase(med, 2);
+    BinarySearchTree(digitos, min, digitos.length()-1);
+  }else if(digitos[med] != digitos[med-1] && med-11 >= 0 ){//si se encuentra una pareja a la izquierda 
+      digitos.erase(med-1, 2);
+      BinarySearchTree(digitos, min, digitos.length()-1);
+  }else if(digitos[med] == digitos[med+1]){//por si no hay pareja a la derecha
+    BinarySearchTree(digitos, med, digitos.length()-1);
+  }else if(digitos[med] == digitos[med-1]){//por si no hay pareja a la izquierda
+    BinarySearchTree(digitos, 0, med);
+  }
+}
+
+
 int main(){
 
-  /* Code Here */
+  int length, min = 0;
+  string digitos;
+
+  cin >> length;
+  cin >> digitos;
+
+  cout << BinarySearchTree(digitos, min, length-1) << endl;
+  cout << digitos << endl;
 
   return 0;
 }

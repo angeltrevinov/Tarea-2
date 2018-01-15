05234c1cc4c2e1c7013925fa6ce846acd576c020
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2018-01-14 19:12:48 -0600

    just one case doesnt work

--- main.cpp
+++ main.cpp
@@ -10,19 +10,20 @@ using namespace std;
 
 int BinarySearchTree(string &digitos, int min, int max){
   int med = (max - min) / 2;
-  if(med == max || med == min)
+  if(!digitos.length())
     return digitos.length();
   else if(digitos[med] != digitos[med+1] && med+1 <= digitos.length()-1 ){//si se encuentra una pareja a la derecha
     digitos.erase(med, 2);
     BinarySearchTree(digitos, min, digitos.length()-1);
-  }else if(digitos[med] != digitos[med-1] && med-11 >= 0 ){//si se encuentra una pareja a la izquierda
+  }else if(digitos[med] != digitos[med-1] && med-1 >= 0 ){//si se encuentra una pareja a la izquierda
       digitos.erase(med-1, 2);
       BinarySearchTree(digitos, min, digitos.length()-1);
   }else if(digitos[med] == digitos[med+1]){//por si no hay pareja a la derecha
-    BinarySearchTree(digitos, med, digitos.length()-1);
-  }else if(digitos[med] == digitos[med-1]){//por si no hay pareja a la izquierda
+    BinarySearchTree(digitos, med+1, digitos.length()-1);
+  }else if(digitos[med] == digitos[med-1]){//por si no hay pareja a la izquierda (se sale del arreglo)
     BinarySearchTree(digitos, 0, med);
   }
+    return digitos.length();
 }
 
 
